fa81a2b6abfe101f8b2bee0ebb547ef8aea51a54
frzrbox <david@freezerbox.dev>
2019-07-20 21:19:02 +0000

    passing

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,23 +5,31 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
+
 module.exports = function(digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
   // do your work here
-  // Check if a number is palidrome
-  function isPalindrome(number) {
-    number.toString().split("");
-    // Split the number at the halfway mark
-    for (let i = 0; i < number.length / 2; i++) {
-      if (number[i] !== number[number.length - 1 - i]) {
-        return false;
+  function checkPalindrome(num) {
+    let numToString = num.toString();
+    const reverse = numToString
+      .split("")
+      .reverse()
+      .join("");
+    return numToString === reverse;
+  }
+  for (let i = 10 ** (digits - 1); i < 10 ** digits; i++) {
+    for (let j = 10 ** (digits - 1); j < 10 ** digits; j++) {
+      if (checkPalindrome(i * j)) {
+        if (i * j > palindromeNumber) {
+          palindromeNumber = i * j;
+          factor_0 = i;
+          factor_1 = j;
+        }
       }
     }
-
-    return true;
   }
 
   // Find all products depending on the digits
